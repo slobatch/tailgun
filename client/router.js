f7b7cc932803fecb5ad44c53600458353ad4c4de
Serge Lobatch <serge@radi.co>
2015-05-13 18:17:48 -0400

    added get unsure page. and made navigation work.

--- client/router.js
+++ client/router.js
@@ -1,7 +1,7 @@
 Router.map(function(){
   this.route('home', {path: '/'} );
   this.route('hello', {path: '/hello'});
-
+  this.route('getUnsub', {path: '/get-unsubscribes'})
   // global notfound
   this.route( 'shotDown', {
     path: '/(.*)'

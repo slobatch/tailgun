ff28c96dfed8b4d55ce63c72889ac5e703bd84e8
Serge Lobatch <serge@radi.co>
2015-05-13 22:10:18 -0400

    working form for submitting and storing mailing list and API key.

--- client/pages/get-unsubscribes/getUnsub.js
+++ client/pages/get-unsubscribes/getUnsub.js
@@ -1,23 +1,51 @@
-// if (Meteor.isClient) {
-//   // This code only runs on the client
-//   Template.getUnsub.helpers({
-//     requests: [
-//       { address: "mail1@domain.com", key:"aslkjhadhgdalkjhblkajh" },
-//       { address: "mail2@domain.com", key:"aslkjhaasaadlkjhblkajh" },
-//       { address: "mail3@domain.com", key:"aslkjhalkjhbasdfhlkajh" }
-//     ]
-//   });
-// }
+Template.getUnsub.helpers({
+  requests: function () {
+    return Requests.find();
+  }
+});
 
-Requests = new Mongo.Collection("requests");
+Template.body.events({
+  "submit .new-task": function (event) {
+    // This function is called when the new task form is submitted
 
-if (Meteor.isClient) {
-  console.log("boop");
-  // This code only runs on the client
-  Template.getUnsub.helpers({
-    requests: function () {
-      console.log(Requests.find());
-      return Requests.find();
-    }
-  });
-}
+    var text = event.target.text.value;
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date() // current time
+    });
+
+    // Clear form
+    event.target.text.value = "";
+
+    // Prevent default form submit
+    return false;
+  }
+});
+
+Template.getUnsub.events({
+  "submit .new-request": function (event) {
+    // This function is called when the new task form is submitted
+    event.preventDefault();
+    console.log("Form Submitted");
+    console.log(event.type);
+    var addressVar = event.target.mailingList.value;
+    console.log(addressVar);
+    var keyVar = event.target.apiKey.value;
+    console.log(keyVar);
+
+    Requests.insert({
+      address: addressVar,
+      key: keyVar,
+      createdAt: new Date() // current time
+    });
+    console.log("boop!");
+
+    // Clear form
+    event.target.mailingList.value = "";
+    event.target.apiKey.value = "";
+
+    // Prevent default form submit
+    return false;
+  }
+});

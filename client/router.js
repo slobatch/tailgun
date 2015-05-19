3fa8045e9c99397bbd34132658b75cd825df08db
Serge Lobatch <serge@radi.co>
2015-05-18 23:31:48 -0400

    Updated router to use Router.route instead of Router.map
    {{pathFor ‘template’}} isn’t working.

--- client/router.js
+++ client/router.js
@@ -2,12 +2,28 @@ Router.configure({
    layoutTemplate: 'tailgun'  //can be any template name
  });
 
-Router.map(function(){
-  this.route('home', {path: '/'} );
-  this.route('hello', {path: '/hello'});
-  this.route('getUnsub', {path: '/get-unsubscribes'})
-  // global notfound
-  this.route( 'shotDown', {
-    path: '/(.*)'
-  });
+// Router.map(function(){
+//   this.route('home', {path: '/'} );
+//   this.route('hello', {path: '/hello'});
+//   this.route('getUnsub', {path: '/get-unsubscribes'})
+//   // global notfound
+//   this.route( 'shotDown', {
+//     path: '/(.*)'
+//   });
+// });
+
+Router.route('/', function(){
+  this.render('home');
+});
+
+Router.route('/hello', function(){
+  this.render('hello');
+});
+
+Router.route('/get-unsubscribes', function(){
+  this.render('getUnsub');
+});
+
+Router.route('/(.*)', function(){
+  this.render('shotDown');
 });
